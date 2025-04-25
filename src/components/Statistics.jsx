22e2c68ddd2dc58fb26c6fc9c50414f5269bed29
Chironjit-Biswas <chironjit2@gmail.com>
2025-04-25 13:06:15 +0600

    new task

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -8,18 +8,17 @@ export const Statistics = () => {
                     <h1 className={'text-[36px] font-extrabold text-[#1E293B] text-center mt-[24px] md:w-2/3 leading-[44px]'}>Work lazily and  achieve the result you want.</h1>
                     <p className={'text-[18px] font-normal text-[#475569] mt-[24px] md:w-1/2 text-center mb-[64px]'}>Liquam id diam maecenas ultricies mi eget mauris pharetra et. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Fermentum iaculis eu non diam.</p>
                 </div>
-                <div className={'md:flex justify-center items-center text-[#4F46E5] md:mx-auto gap-[16px] '}>
-                    <div className={'text-center border-[#CBD5E1] border-r-1'}>
-                        <h1 className={'text-[60px]  font-bold mr-[100px]'}>420%</h1>
+                <div className={'md:flex max-w-[800px] justify-center text-center items-center text-[#4F46E5] md:mx-auto gap-[16px] '}>
+                    <div className={'text-center border-[#CBD5E1] md:border-r-1 md:px-[60px]'}>
+                        <h1 className={'text-[60px]  font-bold '}>420%</h1>
                         <p>Data Efficiency</p>
-
                     </div>
-                    <div className={'text-center border-[#CBD5E1] border-r-1'}>
-                        <h1 className={'text-[60px]  font-bold mr-[100px]'}>708+</h1>
-                        <p>Conversion Rate</p>
+                    <div className={'text-center border-[#CBD5E1] md:border-r-1 md:px-[60px]'}>
+                        <h1 className={'text-[60px]  font-bold  '}>708+</h1>
+                        <p className={'text-center'}>Conversion Rate</p>
                     </div>
-                    <div className={'text-center border-[#CBD5E1] border-r-1 '}>
-                        <h1 className={'text-[60px]  font-bold mr-[100px]'}>1.82M</h1>
+                    <div className={'text-center border-[#CBD5E1] md:border-r-1 md:px-[60px]'}>
+                        <h1 className={'text-[60px]  font-bold'}>1.82M</h1>
                         <p>AI LLM Scale</p>
                     </div>
                 </div>
